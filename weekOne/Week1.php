0dace718fdd3115659dcf929758791e22924e50f
AhmedSulub <sulubahmed72@gmail.com>
2026-09-25 10:15:07 -0700

    PHP-PRACTICE-UPDATE

---
+++ weekOne/Week1.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhP-Parctice</title>
+</head>
+<body>
+    <?php 
+
+    // echo 
+
+   /* echo "Hello Word", " Welcome to Php"; 
+    echo "<br>";
+    echo"myname is Ahmed"    ,"  Iam  Learnin Php"; 
+    echo "<br>";
+  //print
+    print "welcome to Php";
+    echo "<br>";
+    print "welcome to Php "; 
+
+   //Single Qoutes 
+   $x = 100;
+   echo 'Number is $x' 
+
+//    double Qoutes 
+   $y = "Ahmed";
+   echo "myname is $y"; 
+
+
+
+   //String in Php
+    $my_str = "thisisPhp";
+    echo strlen($my_str);
+
+    $str = "my name is ahmed";
+    echo str_word_count($str); 
+
+    //contants
+    
+    define("Name", "Ahmed");
+    print Name;
+
+    define("Age",20);
+    print Age 
+
+    //If Statements
+    $day = "starday";
+    if($day =="starday")
+        echo "its Starday";
+    else 
+        echo "is not starday"; 
+
+    //IF ELSE IF STATEMENTS
+   $grade = 20;
+    if($grade > 95)
+        echo " it isA+";
+    elseif($grade >90 )
+            echo "it is A";
+    elseif($grade>80)
+            echo "it is B+";
+    elseif($grade>75)
+            echo "it is C";
+    elseif($grade>60)
+            echo "it is D";
+    else
+        echo "it is Failled"; */
+    
+    //Switch Statement
+//   $Month = "feb";
+//   switch($Month){
+//     case("Jan");
+//   echo "is Jan";
+//   break;
+//    case("feb");
+//   echo "is feb";
+//   break;
+//    case("march");
+//   echo "is march";
+//   break;
+//    case("April");
+//   echo "is April";
+//   break;
+//    case("may");
+//   echo "is may";
+//   break;
+//    case("jun");
+//   echo "is jun";
+//   break;
+//    case("july");
+//   echo "is july";
+//   default;
+//   echo "its valled month";  
+//   }
+
+  //Another Switch statements
+ /*switch($Color = "kkk"){
+    case ($Color == "Red");
+    echo "Color is Red";
+    break;
+    case ($Color == "Blue");
+    echo "Color is Blue";
+    break;
+    case ($Color == "Green");
+    echo "Color is Green";
+    break;
+    case ($Color == "Yallow");
+    echo "Color is Yellow";
+    break;
+    case ($Color == "Black");
+    echo "Color is Black";
+    break;
+    default;
+    echo "its Not Color";
+ }*/
+
+//while loop Example
+    // $i = 1;
+    // while($i <= 15){
+    //     echo "$i,  ";
+    //     $i++;
+    // }
+
+    //do While loop
+
+
+    // $Result =1;
+    // $n=5;
+    // do{
+    //     $Result *=$n;
+    //     echo "the word of n is: $n <br>";
+    //     $n--;
+
+    // }while ($n>0);
+    // echo $Result;
+
+    //break and continue
+
+    //Nested Loop
+
+    // for($i =1; $i<=3; $i++);
+    //     for($j =1; $j<5; $j++);
+    //   echo("$i * $j =" . "<br>");
+  
+
+//     for ($i = 1; $i <= 3; $i++) {
+
+//     for ($j = 1; $j < 5; $j++) {
+//         echo "$i * $j = " . ($i * $j) . "<br>";
+//     }
+// }
+
+
+   
+
+
+
+
+
+
+    
+    
+    ?>
+    
+</body>
+</html>
---
+++ weekOne/test.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+  //welcome to Php 
+
+// echo "welcome php";
+// echo"<br>"
+// echo "welcome php";
+// print "welcome php";
+// print "welcome php";
+
+
+
+
+
+
+
+
+
+// create variable
+$name = "Ahmed Sulub 20"  ;
+// echo "<br>";
+echo "myname is $name"
+
+//  $Age = 30;
+//  $garde = 10
+
+//  if($Age >20)
+//  elseif($garde <5)
+//     echo "you are nothing";
+//  else
+//   echo  "you are hight";   
+ 
+//if or if else if
+// $grade = 80;
+// if($grade >=40)
+//     echo "excellent";
+
+// elseif($grade >=70)
+//     echo "verry good";
+// else
+//     echo "your not b";
+
+// $marks = 90;
+
+// switch(true)
+//   case ($marks >=80);
+//  echo "excellent"
+//  break;
+//   case ($marks >=70);
+//   echo "good"
+//   break;
+//   default:
+//   echo ("you passed")
+  
+
+
+
+
+
+?>
+</body>
+</html>
